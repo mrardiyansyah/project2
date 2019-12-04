fca725688f77b4ee014815631eb9d1fc7797701e
pramudyaking <pramudyakingdom@gmail.com>
2019-12-05 00:03:04 +0700

    tes

---
+++ tes.php
@@ -0,0 +1,13 @@
+<?php
+
+// header("Content-type: application/json; charset=utf-8");
+
+
+// $url = "https://api.myjson.com/bins/12xmla";
+// $jso = file_get_contents($url);
+// $data = json_decode($jso);
+// var_dump($data);
+
+// $username = $_POST['username'];
+// $pass = $_POST['pass'];
+echo json_encode(['asdf' => 'sd']);
